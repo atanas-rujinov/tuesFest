278475a11156ba114d1417d27d1442a31ac3cda2
atanas-rujinov <atahacr@gmail.com>
2022-06-23 15:40:05 +0300

    smth

--- Capture_the_green_square/Tues_Fest.js
+++ Capture_the_green_square/Tues_Fest.js
@@ -90,7 +90,7 @@ function startGameMenu(){
     ctx.fillStyle = "Black";
     ctx.font = '90px "Black"';
     ctx.textAlign = 'center';
-    ctx.fillText("Capture the green square!", canvas.width / 2, canvas.height / 4);
+    ctx.fillText("PHANTOM HUNTER", canvas.width / 2, canvas.height / 4);
     ctx.font = '50px "Black"';
     ctx.fillText("Start", canvas.width / 2, canvas.height / 2);
     ctx.font = '25px "Black"';
